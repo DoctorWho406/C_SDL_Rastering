78d9dae79833e6217d2959b7223a5b21d5a06802
Federico De Felici <fdf8210@gmail.com>
2023-02-01 15:41:22 +0100

    implemented dda line raster algorithm

--- appl/include/scene.h
+++ appl/include/scene.h
@@ -3,7 +3,21 @@
 #include "SDL.h"
 
 typedef struct {
-    int todo;
+    int width;
+    int height;
+    SDL_Renderer* renderer;
+} screen_t;
+
+typedef struct {
+    Uint8 r;
+    Uint8 g;
+    Uint8 b;
+    Uint8 a;
+} color_t;
+
+
+typedef struct {
+    screen_t* screen;
 } scene_t;
 
 scene_t* scene_create(int w, int h, SDL_Renderer*);
