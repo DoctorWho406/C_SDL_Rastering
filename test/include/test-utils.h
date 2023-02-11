cb10882f52b1db6d93b2d177f7671298ecdee790
Lorenzo Guerrini <lollo.guerrini@gmail.com>
2023-02-11 19:14:45 +0100

    Reformat code

--- test/include/test-utils.h
+++ test/include/test-utils.h
@@ -2,10 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 
-static char* tut_concat_path(const char* path1, const char* path2) 
-{
+static char *tut_concat_path(const char *path1, const char *path2) {
     int dest_size = strlen(path1) + strlen(path2) + 1;
-    char* dest = (char*)malloc(dest_size);
+    char *dest = (char *)malloc(dest_size);
     dest[0] = '\0';
     strcat_s(dest, dest_size, path1);
     strcat_s(dest, dest_size, path2);
